83b2cd7abe5fbcf6df9d825b49ed689f1d041975
altairnik <82442690+altairnik@users.noreply.github.com>
2022-01-25 15:40:50 +0200

    Deleted Stuff.java

--- Stuff.java
+++
@@ -1,7 +0,0 @@
-package homeWork.simpleStore;
-
-public class Stuff extends Products{
-    public Stuff(String mane, String description, int price, int quantity, Type productType) {
-        super(mane, description, price, quantity, productType);
-    }
-}
